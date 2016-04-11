cea547f42bac1ea8961165766a195d30c3582bff
Tejal Bhamre <tbhamre@princeton.edu>
2016-04-10 22:08:52 -0400

    Deleted path from ip3 gen proj file

--- cwf_functions/ip3_make_proj.m
+++ cwf_functions/ip3_make_proj.m
@@ -1,5 +1,4 @@
 n=256;
 K=1000;
-addpath ~/Dropbox/asinger/80S/80s_Tejal/SUGAR/sugar-master/my_scripts/ccwf_v6/final_scripts/expt_ip3
 [projections] = emd5278_proj_full(n,K);
 save /scratch/tbhamre/expt_ip3/ip3_proj.mat  projections
